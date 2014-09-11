dccfcc883e2be991f4a243c0a553e96c2025cbda
Falaleev Maxim <max@studio107.ru>
2014-09-11 17:37:43 +0400

    Up

--- AdminModule.php
+++ AdminModule.php
@@ -6,17 +6,4 @@ use Mindy\Base\Module;
 
 class AdminModule extends Module
 {
-    public function getMenu()
-    {
-        return [
-//            'name' => $this->getName(),
-//            'items' => [
-//                [
-//                    'name' => self::t('Settings'),
-//                    'url' => ['admin.settings'],
-//                    'icon' => 'icon-settings'
-//                ]
-//            ]
-        ];
-    }
 }
