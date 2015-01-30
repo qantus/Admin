5dac1ef0c84c638751ac7fd5beb7503b24291105
Falaleev Maxim <max@studio107.ru>
2015-01-30 14:15:10 +0300

    Trying to refact tables

--- Tables/AdminTable.php
+++ Tables/AdminTable.php
@@ -13,8 +13,86 @@
 
 namespace Modules\Admin\Tables;
 
-class AdminTable 
-{
+use Mindy\Table\Columns\TemplateColumn;
+use Mindy\Table\Table;
+use Modules\Admin\Components\ModelAdmin;
 
+class AdminTable extends Table
+{
+    private $_dynamicColumns = [];
+    /**
+     * @var ModelAdmin
+     */
+    protected $admin;
+
+    public $sortingColumn;
+
+    public $html = [
+        'id' => 'table-main',
+        'data-toggle' => 'checkboxes',
+        'data-range' => 'true'
+    ];
+
+    public function setAdmin(ModelAdmin $admin)
+    {
+        $this->admin = $admin;
+    }
+
+    public function getColumns()
+    {
+        $columns = array_merge($this->_dynamicColumns, [
+            'actions' => [
+                'class' => TemplateColumn::className(),
+                'template' => "admin/admin/_actions.html",
+                'html' => [
+                    'class' => 'actions'
+                ],
+                'extra' => [
+                    'admin' => $this->admin,
+                    'adminClass' => $this->admin->classNameShort(),
+                    'moduleName' => $this->admin->getModule()->classNameShort()
+                ],
+                'virtual' => true
+            ]
+        ]);
+
+        $columns = array_merge([
+            'check' => [
+                'class' => CheckColumn::className(),
+                'length' => $this->count()
+            ]
+        ], $columns);
+
+        if ($this->sortingColumn) {
+            $columns = array_merge([
+                'sorting' => [
+                    'class' => CheckColumn::className(),
+                    'template' => "admin/admin/_sorting_column.html",
+                    'html' => [
+                        'class' => 'sorting',
+                    ],
+                    'virtual' => true
+                ]
+            ], $columns);
+        }
+
+        return $columns;
+    }
+
+    /**
+     * @param $record
+     * @return array
+     */
+    public function getRowHtmlAttributes($record)
+    {
+        return [
+            // For checkboxes
+            'data-pk' => $record->pk
+        ];
+    }
+
+    public function setColumns(array $columns)
+    {
+        $this->_dynamicColumns = $columns;
+    }
 }
- 
--- Tables/CheckColumn.php
+++ Tables/CheckColumn.php
@@ -13,8 +13,51 @@
 
 namespace Modules\Admin\Tables;
 
-class CheckColumn 
-{
+use Mindy\Table\Columns\Column;
 
+class CheckColumn extends Column
+{
+    /**
+     * @var string
+     */
+    public $headCellTemplate = '<th {html}>{out}</th>';
+    /**
+     * @var array
+     */
+    public $html = [
+        'class' => 'check',
+        'align' => 'left'
+    ];
+    /**
+     * @var bool
+     */
+    public $virtual = true;
+    /**
+     * @var int count of objects in table
+     */
+    public $length = 0;
+
+    public function getOut()
+    {
+        return strtr('<input type="checkbox"{disabled} id="check-all"/><label class="checkbox-state" for="check-all"></label>', [
+            '{disabled}' => $this->length == 0 ? ' disabled="disabled"' : ''
+        ]);
+    }
+
+    public function renderHeadCell()
+    {
+        return strtr($this->headCellTemplate, [
+            '{out}' => $this->getOut(),
+            '{html}' => $this->formatHtmlAttributes([
+                'class' => 'check all'
+            ])
+        ]);
+    }
+
+    public function getValue($record)
+    {
+        return strtr('<input type="checkbox" name="models[]" id="check-{pk}" value="{pk}" /><label class="checkbox-state" for="check-{pk}"></label>', [
+            '{pk}' => $record->pk
+        ]);
+    }
 }
- 
